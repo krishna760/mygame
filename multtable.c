ef6ef205c4007cdde25c5c2b208765125e064a31
krish760 <84552004+krishna760@users.noreply.github.com>
2025-06-10 15:51:17 +0545

    Update multtable.c

--- multtable.c
+++ multtable.c
@@ -7,15 +7,15 @@ void printTable(int *multTable, int num, int n){
     }
 printf("******************************************************\n");
     for(int i=0; i<n; i++){
-    printf("%d * %d = %d\n", num, i+1, multTable[i]);
+    printf("%d * %d = %d\n", num, i+1, multTable[i]); // Contains three %d placeholders for integers
 }
 }
 int main(){
-    int multTable[3][10];
-    printTable(multTable[0], 2, 100);
-    printTable(multTable[1], 3, 100);
-    printTable(multTable[2], 4, 100);
-    printTable(multTable[3], 5, 100);
+    int multTable[4][10];  //Declares a 2D array multTable[3][10]. This creates space for 3 rows (tables), each with 10 entries.
+    printTable(multTable[0], 2, 10);
+    printTable(multTable[1], 3, 10);
+    printTable(multTable[2], 4, 10);
+    printTable(multTable[3], 5, 10);
 
     return 0;
 };
